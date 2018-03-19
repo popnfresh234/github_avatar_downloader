185baef7a7f2856a02144285cb4acdc139bfbcb5
Alex Holliday <ajhollid@gmail.com>
2018-03-19 20:46:20 +0000

    Adds download image function

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!!!');
 
@@ -17,8 +18,24 @@ function getRepoContributors(repoOwner, repoName, callback) {
   });
 }
 
+function downloadImageByURL(url, filePath){
+  request(url)
+    .on('error', function(err){
+      console.log(err);
+    })
+    .on('response', function(response){
+      console.log('Response code: ' + response.statusCode);
+    })
+    .pipe(fs.createWriteStream(filePath).on('finish', function(){
+      console.log("Write complete");
+    }));
+}
+
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   result.forEach(function(user){
     console.log(user.avatar_url);
   });
 });
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
