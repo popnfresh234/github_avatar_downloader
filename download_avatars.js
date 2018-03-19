8fb68bcad087268912f20f391b3a95ff355c2c97
Alex Holliday <ajhollid@gmail.com>
2018-03-19 21:23:34 +0000

    Uses numbers for counters

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,13 @@
 var request = require('request');
 var secrets = require('./secrets');
 var fs = require('fs');
+var userCount = 0;
+var currentPosition = 0;
 
 console.log('Welcome to the GitHub Avatar Downloader!!!');
 
 function getRepoContributors(repoOwner, repoName, callback) {
+  console.log('Beginning request');
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -12,7 +15,6 @@ function getRepoContributors(repoOwner, repoName, callback) {
       'Authorization': secrets.GITHUB_TOKEN
     }
   };
-
   request(options, function(err, res, body) {
     callback(err, JSON.parse(body));
   });
@@ -20,19 +22,25 @@ function getRepoContributors(repoOwner, repoName, callback) {
 
 function downloadImageByURL(url, filePath){
   request(url)
-    .on('error', function(err){
+  .on('error', function(err){
+    console.log(err);
+  })
+  .on('response', function(response){
 
-    })
-    .on('response', function(response){
-
-    })
-    .pipe(fs.createWriteStream(filePath).on('finish', function(){
-
-    }));
+  })
+  .pipe(fs.createWriteStream(filePath).on('finish', function(){
+    currentPosition ++;
+    if (currentPosition === userCount){
+      console.log(currentPosition + " Complete");
+    } else {
+      console.log(currentPosition);
+    }
+  }));
 }
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
+  userCount = result.length;
   result.forEach(function(user){
     var path = "avatars/" + user.login + ".jpg";
     downloadImageByURL(user.avatar_url, path);
